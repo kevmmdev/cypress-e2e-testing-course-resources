f23e80a64109e4c2c3b2c7d21d8920390f1937eb
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-08-05 21:06:44 +0800

    15. Selecting By Text - 4min

--- kmm/02 Basics/01 Starting Project/cypress/e2e/basics.cy.js
+++ kmm/02 Basics/01 Starting Project/cypress/e2e/basics.cy.js
@@ -6,4 +6,11 @@ describe("tasks page", () => {
     cy.get(".main-header").find("img");
     // cy.get('.main-header img'); // => also works!
   });
+
+  it("should display the page title", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("h1").should("have.length", 1);
+    cy.get("h1").contains("My Cypress Course Tasks");
+    // cy.contains('My Cypress Course Tasks');
+  });
 });
