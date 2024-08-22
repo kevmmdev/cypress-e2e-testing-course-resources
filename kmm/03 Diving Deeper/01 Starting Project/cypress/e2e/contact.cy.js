4ac3da88a267881a280a04c4d424fe430b9b568f
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-08-22 16:50:28 +0800

    44. Changing Subjects - 7min

--- kmm/03 Diving Deeper/01 Starting Project/cypress/e2e/contact.cy.js
+++ kmm/03 Diving Deeper/01 Starting Project/cypress/e2e/contact.cy.js
@@ -34,22 +34,19 @@ describe("contact form", () => {
     cy.get("@msgInput").focus().blur();
     cy.get("@msgInput")
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
-      });
+      .should("have.attr", "class")
+      .and("match", /invalid/);
 
     cy.get('[data-cy="contact-input-name"]').focus().blur();
     cy.get('[data-cy="contact-input-name"]')
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
-      });
+      .should("have.attr", "class")
+      .and("match", /invalid/);
 
     cy.get('[data-cy="contact-input-email"]').focus().blur();
     cy.get('[data-cy="contact-input-email"]')
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
-      });
+      .should("have.attr", "class")
+      .and("match", /invalid/);
   });
 });
